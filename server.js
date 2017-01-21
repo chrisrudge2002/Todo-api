9b45afb18f9d726f3f977c2dcc58adfa7f824d77
Chris Rudge <chrisrudge2002@gmail.com>
2017-01-21 20:50:18 +0000

    Add todo/user association to all todo routes

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ app.get('/', function(req, res) {
 // GET /todos
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	const query = req.query;
-	let where = {};
+	let where = {
+		userId: req.user.get('id')
+	};
 
 	if (query.hasOwnProperty('q') && query.q.trim().length > 0) {
 		where.description = {
@@ -47,7 +49,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 
 // GET /todos/:id
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
-	db.todo.findById(parseInt(req.params.id, 10)).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: parseInt(req.params.id, 10),
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -60,16 +67,20 @@ app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 // DELETE /todos/:id
 app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
-	db.todo.findById(parseInt(req.params.id, 10)).then(function(todo) {
-		if (!!todo) {
-			todo.destroy(todo);
-			res.status(204).send();
-		} else {
+	db.todo.destroy({
+		where: {
+			id: parseInt(req.params.id, 10),
+			userId: req.user.get('id')
+		}
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted === 0) {
 			res.status(404).send();
+		} else {
+			res.status(204).send();
 		}
 	}, function(e) {
 		res.status(500).send();
-	});
+	})
 });
 
 // POST /todos
@@ -128,7 +139,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 		attributes.completed = body.completed;
 	}
 
-	db.todo.findById(parseInt(req.params.id, 10)).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: parseInt(req.params.id, 10),
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
