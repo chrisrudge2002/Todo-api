870814f725d129d23cca21b81a5d52525ad049c9
Chris Rudge <chrisrudge2002@gmail.com>
2017-01-21 14:55:21 +0000

    Convert email to lowercase

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+"use strict";
+
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('user', {
 		email: {
@@ -13,6 +15,14 @@ module.exports = function(sequelize, DataTypes) {
 			allowNull: false,
 			validate: {
 				len: [7, 100]
+			}
+		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
 			}
 		}
 	});
